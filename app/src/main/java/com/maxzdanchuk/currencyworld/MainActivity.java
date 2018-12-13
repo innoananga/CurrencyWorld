24ae6a281790bb76aa95dafef31763bde27ef50a
MaksimZdanchuk <mzdanchuk@mail.ru>
2018-12-13 15:48:15 +0300

    Update list

--- app/src/main/java/com/maxzdanchuk/currencyworld/MainActivity.java
+++ app/src/main/java/com/maxzdanchuk/currencyworld/MainActivity.java
@@ -2,8 +2,6 @@ package com.maxzdanchuk.currencyworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,17 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Получаем элемент ListView
-        ListView currenciesList = (ListView) findViewById(R.id.currencyList);
-
-        // Получаем ресурс
-        String[] currencies = getResources().getStringArray(R.array.currencies);
-
-        // Создаем адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, currencies);
-
-        // Устанавливаем для списка адаптер
-        currenciesList.setAdapter(adapter);
     }
+
 }
